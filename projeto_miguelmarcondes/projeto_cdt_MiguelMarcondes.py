ea57d879224ff0dede74f6f0b5e17a20326880c8
progamacao-cdtl <miguelsantosmarcondes704@gmail.com>
2026-05-05 17:42:13 -0300

    Update projeto_cdt_MiguelMarcondes.py

---
+++ projeto_miguelmarcondes/projeto_cdt_MiguelMarcondes.py
@@ -0,0 +1,25 @@
+'''
+
+Sistema de Barbearia
+
+'''
+
+
+
+print ("\n Barbearia Andrades - Seja Bem Vindo!")
+
+print("\nPara escolher o serviço digite...")
+
+print ("\n1 - Agendar horário")
+
+print ("\n2 - Serviços disponíveis")
+
+print ("\n3 - Localização e contato")
+
+print ("\n4 - Avalie nosso serviço")
+
+escolha_serviço = input ("\nEscolha o serviço desejado")
+
+
+
+
