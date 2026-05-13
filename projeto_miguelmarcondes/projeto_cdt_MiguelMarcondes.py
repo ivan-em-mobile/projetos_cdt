e7196807aaa3ff4e31765a94fd6751add3b43881
progamacao-cdtl <miguelsantosmarcondes704@gmail.com>
2026-05-13 15:49:30 -0300

    Update projeto_cdt_MiguelMarcondes.py

--- projeto_miguelmarcondes/projeto_cdt_MiguelMarcondes.py
+++ projeto_miguelmarcondes/projeto_cdt_MiguelMarcondes.py
@@ -3,23 +3,65 @@
 Sistema de Barbearia
 
 '''
+while True:
+
+ print ("\n Barbearia Andrades - Seja Bem Vindo!")
+
+ print("\nPara escolher o serviço digite o número correspondente...")
+
+ print ("\n1 - Agendar horário")
+
+ print ("\n2 - Serviços disponíveis")
+
+ print ("\n3 - Localização e contato")
+
+ print ("\n4 - Avalie nosso serviço")
+
+ print ("\n0 - Sair")
+
+escolha_serviço = input ("\nEscolha o serviço desejado:")
+  
+
+if escolha_serviço == '2':
+ 
+ print ("\nCorte masculino - 30R$")
+
+ print ("\nCorte de barba - 25R$ ")
+
+ print ("\nSobrancelha - 20R$ ")
+
+ print ("\nLuzes - 80R$ ")
+
+ print ("\nProgressiva - 100R$")
+
+
+ if escolha_serviço == '3':
+  
+  
+  print ("Endereço:Jardim Macedônia - Rua Póva de Varzim - Nº67 ")
+
+  print ("\nContato: +55 11 91539-7314")
+
+  
+  if escolha_serviço == '4':
+ 
+   print ("Avalie nossos serviços de 1 a 5:")
+
+   print ("\n1 - Péssimo")
+
+   print ("\n2 - Ruim")
+
+   print ("\n3 - Médio")
+
+   print ("\n4 - Bom")
+
+   print ("\n5 - Muito bom")
+
+   avaliar_servico = input ("Ruim:")
 
 
 
-print ("\n Barbearia Andrades - Seja Bem Vindo!")
-
-print("\nPara escolher o serviço digite...")
-
-print ("\n1 - Agendar horário")
-
-print ("\n2 - Serviços disponíveis")
-
-print ("\n3 - Localização e contato")
-
-print ("\n4 - Avalie nosso serviço")
-
-escolha_serviço = input ("\nEscolha o serviço desejado")
 
 
-
+           
 
