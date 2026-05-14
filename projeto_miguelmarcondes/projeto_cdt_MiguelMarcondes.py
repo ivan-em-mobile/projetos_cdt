df329b6aa522c187f797013d18fad68ee5fff142
progamacao-cdtl <miguelsantosmarcondes704@gmail.com>
2026-05-14 16:28:58 -0300

    update

--- projeto_miguelmarcondes/projeto_cdt_MiguelMarcondes.py
+++ projeto_miguelmarcondes/projeto_cdt_MiguelMarcondes.py
@@ -3,61 +3,108 @@
 Sistema de Barbearia
 
 '''
+
 while True:
 
- print ("\n Barbearia Andrades - Seja Bem Vindo!")
+    print("\n Barbearia Andrades - Seja Bem Vindo!")
 
- print("\nPara escolher o serviço digite o número correspondente...")
+    print("\nPara escolher o serviço digite o número correspondente...")
 
- print ("\n1 - Agendar horário")
+    print("\n1 - Agendar horário")
+    print("\n2 - Serviços disponíveis")
+    print("\n3 - Localização e contato")
+    print("\n4 - Avalie nosso serviço")
+    print("\n0 - Sair")
 
- print ("\n2 - Serviços disponíveis")
+    escolha_servico = input("\nEscolha o serviço desejado: ")
 
- print ("\n3 - Localização e contato")
+    if escolha_servico == '1':
 
- print ("\n4 - Avalie nosso serviço")
+        print("\n=== Agendamento de Horário ===")
 
- print ("\n0 - Sair")
+        nome = input("\nDigite seu nome: ")
 
-escolha_serviço = input ("\nEscolha o serviço desejado:")
-  
+        dia = input("Digite o dia do agendamento: ")
 
-if escolha_serviço == '2':
- 
- print ("\nCorte masculino - 30R$")
+        horario = input("Digite o horário desejado: ")
 
- print ("\nCorte de barba - 25R$ ")
+        print("\nAgendamento realizado com sucesso!")
 
- print ("\nSobrancelha - 20R$ ")
+        print(f"\nCliente: {nome}")
 
- print ("\nLuzes - 80R$ ")
+        print(f"Dia: {dia}")
 
- print ("\nProgressiva - 100R$")
+        print(f"Horário: {horario}")
 
+        print("\nObrigado pela preferência!")
 
-elif escolha_serviço == '3':
-  
-  
-  print ("Endereço:Jardim Macedônia - Rua Póva de Varzim - Nº67 ")
+    elif escolha_servico == '2':
 
-  print ("\nContato: +55 11 91539-7314")
+        print("\nCorte masculino - 30R$")
 
-  
-elif escolha_serviço == '4':
- 
-   print ("Avalie nossos serviços de 1 a 5:")
+        print("\nCorte de barba - 25R$")
 
-   print ("\n1 - Péssimo")
+        print("\nSobrancelha - 20R$")
 
-   print ("\n2 - Ruim")
+        print("\nLuzes - 80R$")
 
-   print ("\n3 - Médio")
+        print("\nProgressiva - 100R$")
 
-   print ("\n4 - Bom")
+    elif escolha_servico == '3':
 
-   print ("\n5 - Muito bom")
+        print("\nEndereço: Jardim Macedônia - Rua Póva de Varzim - Nº67")
 
-   avaliar_servico = input ("Ruim:")
+        print("\nContato: +55 11 91539-7314")
+
+    elif escolha_servico == '4':
+
+        print("\nAvalie nossos serviços de 1 a 5:")
+
+        print("\n1 - Péssimo")
+
+        print("\n2 - Ruim")
+
+        print("\n3 - Médio")
+
+        print("\n4 - Bom")
+
+        print("\n5 - Muito bom")
+
+        avaliar_servico = input("\nSua avaliação: ")
+
+        if avaliar_servico == '1':
+
+            print("\nSentimos muito, vamos procurar melhorar :(")
+
+        elif avaliar_servico == '2':
+
+            print("\nSentimos muito, vamos procurar melhorar :(")
+
+        elif avaliar_servico == '3':
+
+            print("\nVamos nos esforçar mais, obrigado!")
+
+        elif avaliar_servico == '4':
+
+            print("\nObrigado, volte sempre!")
+
+        elif avaliar_servico == '5':
+
+            print("\nMuito obrigado, volte sempre!")
+
+        else:
+
+            print("\nAvaliação inválida!")
+
+    elif escolha_servico == '0':
+
+        print("\nSistema encerrado.")
+
+        break
+
+    else:
+
+        print("\nOpção inválida!")
 
 
 
