5928d679458559ceeaa16f881531e0cd812a1b4a
ivan paulino em vocacao <158524537+ivan-em-mobile@users.noreply.github.com>
2026-09-10 15:44:54 -0300

    upgrade-projetos

--- projetos_cdt_seunome/projetos_automacao_seunome.py
+++ projetos_cdt_seunome/projetos_automacao_seunome.py
@@ -90,6 +90,6 @@ def aprovar_todas_pendencias_aluno(nome_aluno):
 # ==========================================
 if __name__ == "__main__":
     # Define aqui o nome do jovem cujas pendências você quer zerar:
-    jovem_para_aprovar = "Lívia Giffony da Silva"
+    jovem_para_aprovar = "Felipe Mendes do Prado"
     
     aprovar_todas_pendencias_aluno(jovem_para_aprovar)
